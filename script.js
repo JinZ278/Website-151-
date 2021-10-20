7bf84678fff2057b1ace907ae87b2bc2c2294b36
Jin Zhang <JinLZhang278@hotmail.com>
2021-10-20 16:03:31 -0400

    Adding a bit of documentation and retrieving state value.

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ function doGelSubmit() {
 	// var color = document.querySelectorAll(`input[name="colors"]:checked`);
 	// console.log(color[0].value);
 	
+	//retrieves contact information
 	var name = document.getElementsByName('customer')[0].value;
 	var address = document.getElementsByName('address')[0].value;
 	var city = document.getElementsByName('city')[0].value;
+	var state = document.getElementsByName('states')[0].value;
 	var zip = document.getElementsByName('zip')[0].value;
 	var phone = document.getElementsByName('phone')[0].value;
 	var email = document.getElementsByName('email')[0].value;
 	
+	//jz if statement that alerts user of missing inputs
 	if (!name) {
 		window.alert("Please enter a name");
 	}
